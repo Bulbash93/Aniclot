7aa2ae995b1c78502a51f83f20ceec8bb81bb08c
Bulbash93 <koriba93@gmail.com>
2025-08-23 12:21:05 +0000

    main

--- js/main.js
+++ js/main.js
@@ -1,30 +1,54 @@
-const swiperHeader = new Swiper('.header__swiper', {
-    effect: 'fade',
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    loop: true,  
+const swiperHeader = new Swiper(".header__swiper", {
+  effect: "fade",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  loop: true,
 });
 
-const swiperNew = new Swiper('.new__swiper', {
-    slidesPerView: 4,
-    spaceBetween: 20,
-    freeMode: true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    }
+const swiperNew = new Swiper(".new__swiper", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  freeMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
-const openBtn = document.getElementById('popup-btn');
-const closeBtn = document.getElementById('closePupup');
-const pupup = document.getElementById('pupup');
+const openBtnHeader = document.getElementById("popup-btn");
+const closeBtnHeader = document.getElementById("closePupup");
+const pupup = document.getElementById("pupup");
 
-openBtn.addEventListener('click', () => {
-    pupup.classList.add('open');
+openBtnHeader.addEventListener("click", () => {
+  pupup.classList.add("open");
 });
 
-closeBtn.addEventListener('click', () => {
-    pupup.classList.remove('open');
+closeBtnHeader.addEventListener("click", () => {
+  pupup.classList.remove("open");
+});
+
+document.querySelectorAll(".menu__link").forEach((a) => {
+  a.addEventListener("click", (evnt) => {
+    document.querySelectorAll(".menu__item").forEach((item) => {
+      if (item.querySelector(".menu__link") !== a) {
+        item.classList.remove("open__dropdown");
+      }
+    });
+    event.isClick = true;
+    a.parentElement.classList.toggle("open__dropdown");
+  });
+});
+
+document.body.addEventListener("click", (event) => {
+  if (
+    event._isClick == true ||
+    event.target.classList.contains("menu__link") == true ||
+    event.target.classList.contains("dropdown__links") == true
+  )
+    return;
+  document.querySelectorAll(".menu__item").forEach((item) => {
+    item.classList.remove("open__dropdown");
+  });
 });
